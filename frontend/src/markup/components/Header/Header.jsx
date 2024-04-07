117c87d61718578c08849c7a0e733925e64665f1
Sa-Dev <Sa-Dev>
2024-04-07 04:46:43 +0300

    frontend folder setup  and start the frontend application

---
+++ frontend/src/markup/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div>
+      <h1>Header Comp</h1>
+    </div>
+  );
+};
+
+export default Header;
---
+++ frontend/src/markup/pages/admin/AddEmployee.jsx
@@ -0,0 +1,10 @@
+
+const AddEmployee = () => {
+  return (
+    <div>
+      <h1>AddEmployee Page</h1>
+    </div>
+  )
+}
+
+export default AddEmployee
