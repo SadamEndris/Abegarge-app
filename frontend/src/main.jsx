c68bf8dc5dbd52197e6e4f45f711eeaf460332d8
Sa-Dev <sadamendris48@gmail.com>
2025-03-07 12:02:24 +0300

    Add Tailwind CSS integration and import styles in main entry point

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import "./assets/template_assets/css/style.css";
+import "./assets/template_assets/css/color.css";
+import "./assets/template_assets/css/bootstrap.css";
+import { BrowserRouter } from "react-router-dom";
 // import the context provider
-import { AuthProvider } from './Context/AuthContext.jsx';
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { AuthProvider } from "./Context/AuthContext.jsx";
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
